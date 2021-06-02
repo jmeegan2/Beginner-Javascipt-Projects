9f0fafd607e977c1bb488f567a04cac0f109a2db
James Meegan <jmeegan2@owlmail.harford.edu>
2021-06-01 23:19:29 -0400

    done project one monkey emoji

--- project-1/app.js
+++ project-1/app.js
@@ -3,15 +3,15 @@ const openFace = document.querySelector('.open')
 
 // Add event listener
 closedFace.addEventListener('click', () => {
-    if(openFace.classList.contains('open')) {
-        openFace.classList.add('active');
-        closedface.classList.remove('active');
+    if (openFace.classList.contains('open')){
+        openFace.classList.add('active'); 
+        closedFace.classList.remove('active');
     }
 });
 
-openFace/addEventListener('click', () => {
-    if(closedface.classList.contains('closed')) {
-        closedFace.classList.add('active')
-        openFace.classList.remove('active')
+openFace.addEventListener('click', () => {
+    if(closedFace.classList.contains('closed')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
     }
-})
+});
